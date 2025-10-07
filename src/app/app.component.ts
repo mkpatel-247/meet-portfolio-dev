206ee397c16806aedf81cfb4fb6023e3837b0fda
mkpatel-247 <meet.codes247@gmail.com>
2025-10-08 00:10:48 +0530

    [MP-2] : Added avater file and integrate tailwind and aos library.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './content/navbar/navbar.component';
 import { FooterComponent } from './content/footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,15 @@ import { HeroComponent } from './hero/hero.component';
 })
 export class AppComponent {
   title = 'meet-portfolio';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  async ngAfterViewInit() {
+    // run only in browser
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    // dynamic import so 'aos' code runs only in browser
+    const AOS = (await import('aos')).default;
+    AOS.init({ duration: 600, once: false });
+  }
 }
